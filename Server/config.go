f4aca59fd8532a9234ef1a709c2e699f408c3963
Aria Diamond <jwdiamon@ucsc.edu>
2021-05-08 16:36:27 -0700

    Finished config parsing

--- Server/config.go
+++ Server/config.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"strings"
 )
@@ -12,30 +11,43 @@ func ParseConfig(folder string) {
 	contents, err := ioutil.ReadFile(folder + ".httpconfig")
 	if err != nil {
 		return
-	}	
+	}
 
-	list := strings.Split(string(contents), "\n")
-
+	list       := strings.Split(string(contents), "\n")
+	fileConfig := make(map[string][]string)
 	// check for comments (#)
 	for _, val := range(list) {
 		if len(val) == 0 {
 			continue
 		}
 		cmtIdx := strings.Index(val, "#")
+		var line string
 		if cmtIdx > 0 {
-			Forbidden[val[:cmtIdx]] = true
+			line = val[:cmtIdx]
 		} else if cmtIdx == -1 { //there is no comment
-			Forbidden[val] = true
+			line = val
 		}
+		url, files := parseLine(line)
+		if len(url) == 0 {
+			Warn("Unable to parse:\n" + line)
+			continue
+		}
+		fileConfig[url] = files
 	}
-
+	Config[folder] = fileConfig
 }
 
 func parseLine(line string) (string, []string) {
-	splitter := strings.Contains(line, "=>")
+	splitter := strings.Index(line, "=>")
 	if splitter == -1 {
 		//error
+		return "", nil
 	}
-	//strings.TrimSpace
-
+	url   := strings.TrimSpace(line[:splitter])
+	files := strings.Split(line[splitter + 1:], ",")
+	// we need error checking to have non-zero lengths?
+	for idx, val := range(files) {
+		files[idx] = strings.TrimSpace(val)
+	}
+	return url, files
 }
